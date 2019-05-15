e995b95cadb497623dd0d67212cf7ab0c3078d55
ASUS <a13289@alunos.ipca.pt>
2019-05-15 09:54:10 +0100

    Merge branch 'master' of C:\Users\ASUS\MultisensorApp with conflicts.

--- app/src/main/java/com/example/multisensorapp/MainActivity.java
+++ app/src/main/java/com/example/multisensorapp/MainActivity.java
@@ -9,17 +9,11 @@ import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
-    Button buttonLogin;
-    EditText editTextEmail;
-    EditText editTextPassword;
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,44 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        buttonLogin = (Button)findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        buttonStart=findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
             }
         });
-    }
-
-    public void login() {
-        VolleyHelper.userLogin(
-                this,
-                editTextEmail.getText().toString(),
-                editTextPassword.getText().toString(),
-                new VolleyHelper.OnResponseListener() {
-                    @Override
-                    public void onSuccess(JSONObject jsonObject) {
-                        // Testar os vários tipos de resposta
-                        if (jsonObject.has("status")){
-                            try {
-                                if (jsonObject.getString("status").contains("error")){
-                                    Toast.makeText(MainActivity.this,"No internet connection",Toast.LENGTH_LONG).show();
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(MainActivity.this,"No internet connection",Toast.LENGTH_LONG).show();
-                            }
-                        }else if (jsonObject.has("id")){
-                            // Guardar dados do utilisador
-                            Intent intent = new Intent(MainActivity.this, Menu.class);
-                            startActivity(intent);
-                        }else {
-                            Toast.makeText(MainActivity.this,"No user name or password",Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-
     }
 }
